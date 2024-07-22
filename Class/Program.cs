f0cb0a9e96f5c8fca7b9e4f6f607e6cda107b0d2
iyovory <97534410+iyovory@users.noreply.github.com>
2024-07-22 15:53:32 +0900

    study: 객체지향(OOP)과 클래스

---
+++ Class/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace ConsoleApp3
+{
+    #region Chracter 클래스 선언하기
+    /*
+        Chracter 클래스를 선언하고, 클래스가 가져야 할 필드와 메서드는 아래와 같다.
+        필드:
+        레벨
+        체력
+        이동 속도
+        공격력
+
+        메서드:
+        전진
+        후진
+        좌회전(90도)
+        우회전(90도)
+        공격
+        피격
+     */
+    internal class Program
+    {
+        class Chracter
+        {
+            public int level;
+            public int hp;
+            public int speed;
+            public int ap;
+
+            public void Advance()
+            {
+
+            }
+            public void Reverse()
+            {
+
+            }
+            public void TurnLeft90()
+            {
+
+            }
+            public void TurnRight90()
+            {
+
+            }
+            public void Attack()
+            {
+
+            }
+            public void BingAttacked()
+            {
+
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    #endregion
+}
